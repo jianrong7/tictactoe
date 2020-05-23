3765d4dae3acd62617016b4a68bfae3fb3e25a8b
Loh Jian Rong <ipwnjianrong@gmail.com>
2020-05-23 15:33:19 +0800

    Added cursor css

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
-const Gameboard = () => {
-    let state = new Array(3);
-    return state;
-};
+const Gameboard = (() => {
+    const cellElements = document.querySelectorAll('.cell');
+    const cells = [];
+  
+    cellElements.forEach(cell => cells.push(cell));
+  
+    return { cells };
+  })();
 const playerFactory = (name) => {
     let score = 0;
     let playerName = name;
@@ -12,8 +16,19 @@ const playerFactory = (name) => {
     }
     return { win };
 }
-const game = (playerOne, playerTwo) => {
-
+const game = (playerOne, playerTwo) => {   
+    const PLAYER_ONE_MARK = 'X';
+    const PLAYER_TWO_MARK = 'O';
+    const WINNING_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
 }
 let x = playerFactory('x');
 let o = playerFactory('o');
