05854ede6a6e8fbaa185908d3e841fb6c0e5a472
jianrong7 <ipwnjianrong@gmail.com>
2021-04-07 16:16:27 +0800

    Update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,29 @@
-const X_CLASS = 'x';
-const O_CLASS = 'o';
-const WINNING_COMBINATIONS = [
+"use strict";
+
+const playerFactory = (choice) => {
+    return { choice };
+};
+const gameBoard = (() => {
+    const board = ['', '', '', '', '', '', '', '', ''];
+    return { board };
+})();
+const displayController = (() => {
+
+})();
+const gameController = (() => {
+    const playerOne = playerFactory('x');
+    const playerTwo = playerFactory('o');
+    let round = 1;
+    let isOver = false;
+    const playRound = () => {
+        console.log(getCurrentPlayerSign())
+
+    }
+    const getCurrentPlayerSign = () => {
+        return round % 2 === 1 ? playerOne.choice : playerTwo.choice;
+    }
+    const checkWinner = () => {
+        const WINNING_COMBINATIONS = [
             [0, 1, 2],
             [3, 4, 5],
             [6, 7, 8],
@@ -9,4 +32,8 @@ const WINNING_COMBINATIONS = [
             [2, 5, 8],
             [0, 4, 8],
             [2, 4, 6],
-          ];
+        ];
+    }
+
+    return { playRound }
+})();
